f8b35f2ef7cdedc94ca8c95d0cbcc1bb0d9eb31a
afdal.m <afdal2404@gmail.com>
2020-08-20 18:04:56 +0700

    install library select2, implementasi select2 pada pencarian realtia

--- resources/js/ongkir.js
+++ resources/js/ongkir.js
@@ -16,3 +16,24 @@ $('select[name="province_origin"]').on('change', function () {
         $('select[name="city_origin"]').empty();
     }
 });
+
+$('#city_destination').select2({
+    ajax: {
+        url: "/api/cities",
+        type: "POST",
+        dataType: "json",
+        delay: 150,
+        data: function (params) {
+            return {
+                _token: $('meta[name="csrf-token"]').attr('content'),
+                search: $.trim(params.term)
+            }
+        },
+        processResults: function (response) {
+            return {
+                results: response
+            };
+        },
+        cache: true
+    }
+});
